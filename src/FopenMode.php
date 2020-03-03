6381243c10789dbb3358b37b5c6912bcd2ece1bb
Christer Edvartsen <cogo@starzinger.net>
2020-03-03 20:33:36 +0100

    Add class for fopen-mode data

---
+++ src/FopenMode.php
@@ -0,0 +1,84 @@
+<?php declare(strict_types=1);
+namespace TestFs;
+
+class FopenMode {
+    /**
+     * @var bool
+     */
+    private $read;
+
+    /**
+     * @var bool
+     */
+    private $write;
+
+    /**
+     * @var int
+     */
+    private $offset;
+
+    /**
+     * @var bool
+     */
+    private $truncate;
+
+    /**
+     * @var bool
+     */
+    private $create;
+
+    /**
+     * @var bool
+     */
+    private $binary;
+
+    /**
+     * @var bool
+     */
+    private $text;
+
+    /**
+     * Class constructor
+     *
+     * @param string $mode Open mode
+     * @param bool $extended Extended mode
+     * @param ?string $extra Extra options
+     */
+    public function __construct(string $mode, bool $extended, ?string $extra) {
+        $this->read     = 'r' === $mode || $extended;
+        $this->write    = in_array($mode, ['w', 'a', 'x', 'c']) || $extended;
+        $this->offset   = 'a' === $mode ? SEEK_END : 0;
+        $this->truncate = 'w' === $mode;
+        $this->create   = in_array($mode, ['w', 'a', 'x', 'c']);
+        $this->binary   = 'b' === $extra;
+        $this->text     = 't' === $extra;
+    }
+
+    public function read() : bool {
+        return $this->read;
+    }
+
+    public function write() : bool {
+        return $this->write;
+    }
+
+    public function create() : bool {
+        return $this->create;
+    }
+
+    public function truncate() : bool {
+        return $this->truncate;
+    }
+
+    public function offset() : int {
+        return $this->offset;
+    }
+
+    public function binary() : bool {
+        return $this->binary;
+    }
+
+    public function text() : bool {
+        return $this->text;
+    }
+}
---
+++ tests/FopenModeTest.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+namespace TestFs;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass TestFs\FopenMode
+ */
+class FopenModeTest extends TestCase {
+    public function getModeData() : array {
+        return [
+            [
+                'mode'             => 'r',
+                'extended'         => true,
+                'extra'            => null,
+                'expectedRead'     => true,
+                'expectedWrite'    => true,
+                'expectedOffset'   => 0,
+                'expectedTruncate' => false,
+                'expectedCreate'   => false,
+                'expectedBinary'   => false,
+                'expectedText'     => false
+            ],
+            [
+                'mode'             => 'w',
+                'extended'         => false,
+                'extra'            => null,
+                'expectedRead'     => false,
+                'expectedWrite'    => true,
+                'expectedOffset'   => 0,
+                'expectedTruncate' => true,
+                'expectedCreate'   => true,
+                'expectedBinary'   => false,
+                'expectedText'     => false
+            ],
+        ];
+    }
+
+    /**
+     * @covers ::__construct
+     * @dataProvider getModeData
+     */
+    public function testCorrectlyCreatesInstance(string $mode, bool $extended, ?string $extra, bool $expectedRead, bool $expectedWrite, int $expectedOffset, bool $expectedTruncate, bool $expectedCreate, bool $expectedBinary, bool $expectedText) : void {
+        $mode = new FopenMode($mode, $extended, $extra);
+
+        $this->assertSame($expectedRead, $mode->read(), 'Incorrect read value');
+        $this->assertSame($expectedWrite, $mode->write(), 'Incorrect write value');
+        $this->assertSame($expectedOffset, $mode->offset(), 'Incorrect offset value');
+        $this->assertSame($expectedTruncate, $mode->truncate(), 'Incorrect truncate value');
+        $this->assertSame($expectedCreate, $mode->create(), 'Incorrect create value');
+        $this->assertSame($expectedBinary, $mode->binary(), 'Incorrect binary value');
+        $this->assertSame($expectedText, $mode->text(), 'Incorrect text value');
+    }
+}
